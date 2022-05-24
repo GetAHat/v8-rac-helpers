a47ca1420a59d254e47909dd4aef5f01af911b00
Andrey Fomin <andref@rarus.ru>
2022-05-24 22:02:25 +0300

    rm'd assignments, added commented out options for cluster auth

--- createDatabasePoolFromFile.sh
+++ createDatabasePoolFromFile.sh
@@ -17,7 +17,11 @@ clusterid= # add cluster id here
 platform_path=/opt/1C/v8.3/x86_64
 db_server= # add db server addr here. If it doesn't use standart port, you can specify it with "127.0.0.1 port=1433"
 ras_addr= # add ras server to connect to like this: 127.0.0.1:1545
-db_list=$1
+db_type=MSSQLServer
+cluster_user= # add cluster admin username here
+cluster_passwd= # add cluster admin password here
+
+
 
 # Auth data for mssql
 db_user= # add mssql username here
@@ -25,22 +29,26 @@ db_passwd= # add mssql user password here
 
 db_description="CreatedWithScript" # you can provide any description you like. Mind the spaces. Or rather lack of.
 
+db_list=$1
 rac=$platform_path/rac
 
 
 printf "This is a simple script to automate db creation in 1cv8.\n"
 printf "***ADD YOUR PARAMETERS BEFORE USING IT***\n\n"
 
-while read dbname; do 
-    database_name=$dbname
+while read database_name; do 
 
     printf "v8 base willbe called $database_name\n"
     printf "mssql db will be called $database_name\n\n"
 
-    $rac infobase $ras_addr --cluster=$clusterid create \
+    $rac infobase $ras_addr --cluster=$clusterid \ 
+    # --cluster-user=$cluster_user \ #
+                                     #  << uncomment this for cluster auth
+    #--cluster-pwd=$cluster_passwd \ # 
+    create \
         --create-database \
         --name=$database_name \
-        --dbms=MSSQLServer \
+        --dbms=$db_type \
         --db-server=$db_server \
         --db-name=$database_name \
         --db-user=$db_user \
